c67ebae51091ab7e835faa97d7197e41f8980be7
Evan <evan.w.mclaughlin@gmail.com>
2021-07-22 10:58:41 -0400

    Update license

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 /**
-Copyright (c) 2020-present, Eaton
- 
+Copyright (c) 2021-present, Eaton
+
 All rights reserved.
- 
+
 This code is licensed under the BSD-3 license found in the LICENSE file in the root directory of this source tree and at https://opensource.org/licenses/BSD-3-Clause.
 **/
 export { blueDarkTheme as blueDark } from './blueDarkTheme';
