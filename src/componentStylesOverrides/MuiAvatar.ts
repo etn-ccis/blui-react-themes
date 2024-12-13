6e52285f6148f61b72af55660738f8ca08eff0ac
shubham-eaton <ShubhamkVishwakarma@eaton.com>
2024-12-13 12:43:26 +0530

    Added dark mode color for avatar

--- src/componentStylesOverrides/MuiAvatar.ts
+++ src/componentStylesOverrides/MuiAvatar.ts
@@ -1,10 +1,18 @@
+import { BLUIColors } from "@brightlayer-ui/colors";
 import { Components, CssVarsTheme, Theme } from "@mui/material/styles";
+import Color from "color";
 
 export default {
     styleOverrides: {
         colorDefault: ({ theme }) => ({
             backgroundColor: theme.vars.palette.primary.light,
             color: theme.vars.palette.primary.main,
+            ...theme.applyStyles("dark", {
+                backgroundColor: Color(BLUIColors.black[50])
+                    .alpha(0.1)
+                    .string(),
+                color: theme.vars.palette.text.primary,
+            }),
         }),
     },
 } as Components<
